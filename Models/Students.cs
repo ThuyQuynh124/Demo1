4bb12be0c7defe4b72d448be97fe19e605e228db
LE THUY QUYNH <1724010123@student.humg.edu.vn>
2021-10-25 14:49:18 +0700

    create_class_Std

---
+++ Models/Students.cs
@@ -0,0 +1,9 @@
+using System;
+namespace DemoNetCore.Models
+{
+    public class Students
+    {
+        public String StudenID { get; set; }
+        public string StudentName { get; set; }
+    }
+}
